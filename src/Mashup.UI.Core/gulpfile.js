28cbb8761fc0b4f5d3eadb28af125ee4963d8c75
Dax <daaawx@gmail.com>
2020-05-02 22:00:00 +0200

    import plugins

--- src/Mashup.UI.Core/gulpfile.js
+++ src/Mashup.UI.Core/gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', async function() {
   // Default task here
